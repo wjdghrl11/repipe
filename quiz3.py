64ec114749345f95d47158338f09fade7c88472a
wjdghrl11 <wjdghrl26@gmail.com>
2021-07-15 22:47:59 +0800

    re1

---
+++ quiz3.py
@@ -0,0 +1,23 @@
+print(100)
+
+print(100 + 10)
+
+print(((100 + 10) / 10) - 10)
+
+print((((100 + 10) / 10) - 10) * 10)
+
+a = 100
+print(a)
+
+b = a + 10
+print(b)
+
+c = b / 10
+print(c)
+
+d = c - 10
+print(d)
+
+e = d * 10
+print(e)
+
---
+++ quiz4.py
@@ -0,0 +1,26 @@
+# 아래 코드를 변수를 이용해 간단하게 바꿔주세요.
+print(1)
+
+print(1 + 2)
+
+print(1 + 2 + 3)
+
+print(1 + 2 + 3 + 4)
+
+print(1 + 2 + 3 + 4 + 5)
+
+
+a = 1
+b = a + 2
+c = b + 3
+d = c + 4
+e = d + 5
+print(a)
+
+print(b)
+
+print(c)
+
+print(d)
+
+print(e)
